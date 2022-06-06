80b967f8fb9efa2c1ef85b05800d2a9706d691e3
NemoFlip <artgamer3163@gmail.com>
2022-06-06 10:53:42 +0300

    Initial Commit

---
+++ UILabelTestPreview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILabelTestPreview
+//
+//  Created by Артем Хлопцев on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
